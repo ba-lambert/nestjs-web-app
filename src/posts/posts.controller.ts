b5daff5047e071642c72bf740a2cf1d4257e5a30
ba-lambert <mugwanezalambert12b@gmail.com>
2024-04-10 18:17:59 +0200

    crud oops for post

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete,Request, UseGuards, Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, UseGuards, Res } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
@@ -7,30 +7,30 @@ import { Response } from 'express';
 
 @Controller('posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(private readonly postsService: PostsService) { }
   @UseGuards(AuthenticatedGuard)
   @Post()
-  create(@Body() createPostDto: CreatePostDto,@Request() req) {
-    return this.postsService.create(req.session.passport.user.userId,createPostDto);
+  create(@Body() createPostDto: CreatePostDto, @Request() req) {
+    return this.postsService.create(req.session.passport.user.userId, createPostDto);
   }
   @UseGuards(AuthenticatedGuard)
   @Get()
-  findAll(@Request() req,@Res() res:Response) {
-    return this.postsService.findAll(req.session.passport.user.userId,res);
+  findAll(@Request() req, @Res() res: Response) {
+    return this.postsService.findAll(req.session.passport.user.userId, res);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postsService.findOne(+id);
+  findOne(@Param('id') id: string,@Request()req,@Res() res:Response) {
+    return this.postsService.findOne(req.session.passport.user.userId,id,res);
   }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postsService.update(+id, updatePostDto);
+  @UseGuards(AuthenticatedGuard)
+  @Patch(':postId')
+  update(@Param('postId') postId: string, @Body() updatePostDto: UpdatePostDto, @Request() req, @Res() res:Response) {
+    return this.postsService.update(req.session.passport.user.userId, updatePostDto, postId,res);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postsService.remove(+id);
+  remove(@Param('id') id: string,@Res() res:Response,@Request() req) {
+    return this.postsService.remove(id,req.session.passport.user.userId,res);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -4,6 +4,7 @@ import { UpdatePostDto } from './dto/update-post.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
 import { Repository } from 'typeorm';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class PostsService {
@@ -41,15 +42,51 @@ export class PostsService {
 
 
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(userId:string,post,res) {
+    try{
+      const findPost = await this.postRepository.findOne({where:{auth:{id:userId},postId:post}})
+      if(!findPost){
+        return res.status(HttpStatus.NOT_FOUND).json({message:"Post not found"})
+      }
+      return res.status(HttpStatus.ACCEPTED).json({message:"FoundPost",post:findPost})
+    }catch(err){
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json("Some thing went wrong")
+    }
+    return ;
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: any, updatePostDto: UpdatePostDto,postId:any,res) {
+    try{
+      const findPost = await this.postRepository.findOne({ where: { auth:{id:id} , postId: postId } });
+      if(!findPost){
+        console.log('Post not found')
+      }
+      if(updatePostDto.author){
+        findPost.author = updatePostDto.author
+      }
+      if(updatePostDto.content){
+        findPost.content = updatePostDto.content
+      }
+      if(updatePostDto.header){
+        findPost.header = updatePostDto.header
+      }
+      const savePost = await this.postRepository.save(findPost)
+      return res.status(HttpStatus.ACCEPTED).json({post:savePost})
+    }catch(err){
+      throw new NotFoundError(`Some thing went wrong ${err}`)
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: string,userId:string,res) {
+    try{
+      const findPost = await this.postRepository.findOne({where:{auth:{id:userId},postId:id}})
+      if(!findPost){
+        return res.status(HttpStatus.NOT_FOUND).json({message:"Post not found"})
+      }
+      await this.postRepository.remove(findPost)
+      return res.status(HttpStatus.OK).json({message:"Post deleted Successfully"})
+    }catch(error){
+
+    }
   }
 }
